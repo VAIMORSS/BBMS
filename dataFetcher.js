b2e1bef4a44c81ed37eff8add0034ebd1542f15b
Vaibhav <vamoradiya@myseneca.ca>
2018-11-21 20:54:18 -0500

    added database

--- dataFetcher.js
+++ dataFetcher.js
@@ -1,4 +1,35 @@
 var fs = require('fs');
+var sequelize = require('sequelize');
+
+var Sequelize = new sequelize('d558e7l0prgtpo', 'bhttoyjbvistmo', '24f9a4f901a9929509d983b24197af50a9240d2ceb62eb6eb91aa6beb656c425', {
+    host: 'ec2-23-21-147-71.compute-1.amazonaws.com',
+    dialect: 'postgres',
+    port: '5432',
+    dialectOptions: {
+        ssl: true
+    }
+});
+
+Sequelize.authenticate.then(() => {
+    console.log("Database conncted successfully!!!");
+}).catch((err) => {
+    console.log(err);
+});
+
+const Person = Sequelize.define('Person', {
+    PersonNum: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    firstName: Sequelize.STRING,
+    lastName: Sequelize.STRING,
+    std: Sequelize.INTEGER,
+    school: Sequelize.STRING
+}, {
+        updatedAt: false
+    });
+
 
 var person = [];
 var departments = [];
@@ -6,17 +37,11 @@ var totalPerson = 0;
 
 module.exports.initialize = () => {
     return new Promise((resolve, reject) => {
-        try {
-            fs.readFile('./data/person.json', (err, data) => {
-                if (err) throw err;
-                person = JSON.parse(data);
-                totalPerson = person.length;
+            Sequelize.sync().then((Person) => {
+                resolve();
+            }).catch((err) => {
+                reject("unable to read the files");
             });
-        } catch (ex) {
-            reject("unable to read the files");
-        }
-
-        resolve("Successfully, connected to the file!!");
     });
 }
 
@@ -34,14 +59,13 @@ module.exports.getAllPerson = () => {
 }
 
 module.exports.addPerson = (personData) => {
-    
     return new Promise((resolve, reject) => {
         person.push(personData);
         console.log("data added");
-        if(person.length==0){
+        if (person.length == 0) {
             reject("no record found");
         }
-        for(var i=0;i<person.length;i++){
+        for (var i = 0; i < person.length; i++) {
             console.log("from loop");
             console.log(person);
         }
